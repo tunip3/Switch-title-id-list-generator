73e5d55a50d71b6bdd540e9e5f9a1d5e96e68207
Ronald Diemicke <Ronald Diemicke>
2018-07-25 23:20:43 -0500

    Revert "Updated to allow filtering based on language code"
    This reverts commit e5da6b7e80eaab1408e9aa56017d34e3d99dba8c.

--- switch title id list generator.py
+++ switch title id list generator.py
@@ -7,45 +7,29 @@ tree = ET.parse('db.xml')
 root = tree.getroot()
 
 count = 0
-loop1=True
-loop2=True
+loop=True
 
-while loop1 == True:
+while loop == True:
     filename = input("do you want to set a custom name for the title id list \nor leave it as the default (eNXhop.txt for use with eNXhop) \ncustom or default:")
     if filename == "custom":
         filename = input("please enter a name for the output including a file extension: ")
-        loop1=False
+        loop=False
     if filename == "default" or not filename:
         filename = "eNXhop.txt"
-        loop1=False
+        loop=False
     else:
         print("please enter custom or default\n")
 
-while loop2 == True:
-    langCode = input("Do you only want to pull titles that have a specific language code? If so, enter that language code (for example, en)?")
-    if langCode:
-        loop2=False
-    if not langCode:
-        langCode = ""
-        loop2=False
-
 file = open(filename,"w")
 file = open(filename,"r+")
-for release in root:
-  isCorrectLangCode=False
-  for child in release:
-    if child.tag == "languages":
-      if langCode == "" or langCode in child.text:
-        isCorrectLangCode=True
-        lang= str(child.text)
-        print(lang)
-      else:
-        isCorrectLangCode=False
-    if child.tag == "titleid" and isCorrectLangCode:
-      tid= str(child.text)
-      print(tid)
-      file.write(tid)
-      file.write("\n")
+for titleid in root.iter('titleid'):
+    if count >= 1:
+        file.write("\n")
+    else:
+        count+=1
+    tid= titleid.text
+    print(tid)  
+    file.write(tid)
     
 file.close()
 os.remove("db.xml")
