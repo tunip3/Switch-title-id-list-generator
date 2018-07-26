be07960e6e4936a71f9b287b79752f95b5c51451
Ronald Diemicke <Ronald Diemicke>
2018-07-25 23:22:08 -0500

    Allow filtering based on language code
    Allowed for a second prompt for user to filter by a given two character language code. If no language is given, all results are dumped.

--- switch title id list generator.py
+++ switch title id list generator.py
@@ -7,29 +7,45 @@ tree = ET.parse('db.xml')
 root = tree.getroot()
 
 count = 0
-loop=True
+loop1=True
+loop2=True
 
-while loop == True:
+while loop1 == True:
     filename = input("do you want to set a custom name for the title id list \nor leave it as the default (eNXhop.txt for use with eNXhop) \ncustom or default:")
     if filename == "custom":
         filename = input("please enter a name for the output including a file extension: ")
-        loop=False
-    if filename == "default" or not filename:
+        loop1=False
+    if filename == "default":
         filename = "eNXhop.txt"
-        loop=False
+        loop1=False
     else:
         print("please enter custom or default\n")
 
+while loop2 == True:
+    langCode = input("Do you only want to pull titles that have a specific language code? If so, enter that language code (for example, en)?")
+    if langCode:
+        loop2=False
+    if not langCode:
+        langCode = ""
+        loop2=False
+
 file = open(filename,"w")
 file = open(filename,"r+")
-for titleid in root.iter('titleid'):
-    if count >= 1:
-        file.write("\n")
-    else:
-        count+=1
-    tid= titleid.text
-    print(tid)  
-    file.write(tid)
+for release in root:
+  isCorrectLangCode=False
+  for child in release:
+    if child.tag == "languages":
+      if langCode == "" or langCode in child.text:
+        isCorrectLangCode=True
+        lang= str(child.text)
+        print(lang)
+      else:
+        isCorrectLangCode=False
+    if child.tag == "titleid" and isCorrectLangCode:
+      tid= str(child.text)
+      print(tid)
+      file.write(tid)
+      file.write("\n")
     
 file.close()
 os.remove("db.xml")
